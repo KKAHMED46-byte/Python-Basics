508bcc466a1c026448ab4f17ba884c6e5182e9dc
KKAHMED46-byte <khajakhutubuddinahmed46@gmail.com>
2025-09-20 15:05:12 +0530

    Update 10. Dictionary.py

--- 10. Dictionary.py
+++ 10. Dictionary.py
@@ -15,10 +15,10 @@
 # Characteristics of Dictionary:
 # key-value pairs: 
 # Every entry of Dictionary's element is a pair: keys and values. 
-# { "name":"nandhan"}
+# { "name":"kkahmed"}
 # Unique Keys: 
 # example: 
-# A = {"n":"Nandhan","n":"Anurag"}
+# A = {"n":"kkahmed","n":"Anurag"}
 # print(A) # {"n":"Anurag"}
 # Keys must be immutable: 
 # keys can be(valid keys): integer,float,string
@@ -32,14 +32,14 @@
 # Creating Dictionary:
 # Normal Dictionary:
 # BioData = {
-#     "Name":"Nandhan",
-#     "RollNo":66,
-#     "Branch":"CSC",
-#     "Place":"Kamareddy"
+#     "Name":"kkahmed",
+#     "RollNo":32,
+#     "Branch":"CSE-CS",
+#     "Place":"siddipet"
 # }
 # print(BioData)
 # Dictionary using Constructor: 
-# BioData1 = dict( Name="Nandhan",Roll_No=66,Branch="CSC")
+# BioData1 = dict( Name="kkahmed",Roll_No=32,Branch="CSE-CS")
 # print(BioData1)
 # Empty Dictionary:
 # E_D = {}
@@ -48,37 +48,37 @@
 # -> To access an element we use key names inside the square brackets [] or we can use get() method.
 
 # BioData = {
-#     "Name":"Nandhan",
-#     "RollNo":66,
-#     "Branch":"CSC",
-#     "Place":"Kamareddy"
+#     "Name":"KKAHMED",
+#     "RollNo":32,
+#     "Branch":"CSE-CS",
+#     "Place":"SIDDIPET"
 # } 
 # square brackets []
-# print(BioData["Name"]) # Nandhan
-# print(BioData["RollNo"]) # 66
-# print(BioData["Branch"]) # CSC
-# print(BioData["Place"]) # Kamareddy
+# print(BioData["Name"]) # KKAHMED
+# print(BioData["RollNo"]) # 32
+# print(BioData["Branch"]) # CSE-CS
+# print(BioData["Place"]) # SIDDIPET
 # # using get() method:
-# print(BioData.get("Name")) # Nandhan
-# print(BioData.get("RollNo")) # 66
-# print(BioData.get("Branch")) # CSC
-# print(BioData.get("Place")) # Kamareddy
+# print(BioData.get("Name")) # KKAHMED
+# print(BioData.get("RollNo")) # 32
+# print(BioData.get("Branch")) # CSE-CS
+# print(BioData.get("Place")) # SIDDIPET
 
 # Adding and Updating Dictionary:
 # Adding: You can insert a new key-value pari into a dictionary.
 # Updating: You can update or change the values using exsisted keys in the dictionary. 
 # BioData = {
-#     "Name":"Nandhan",
-#     "RollNo":66,
-#     "Branch":"CSC",
-#     "Place":"Hyderabad"
+#     "Name":"KKAHMED",
+#     "RollNo":32,
+#     "Branch":"CSE-CS",
+#     "Place":"SIDDIPET"
 # } 
 # update:
-# BioData["RollNo"] = 16
-# BioData["Name"] = "Nikhil"
+# BioData["RollNo"] = 32
+# BioData["Name"] = "KKAHMED"
 # print(BioData)
 # # add: 
-# BioData["Role"] = "Software developer"
+# BioData["Role"] = "ETHICAL HACKER"
 # print(BioData)
 
 # Removing in Dictionary:
@@ -86,12 +86,12 @@
 # pop(),popitem(),clear(), del(delete)
 
 # BioData = {
-#     "Name":"Nandhan",
-#     "RollNo":66,
-#     "Blood grp":"B+",
-#     "Branch":"CSC",
-#     "Place":"Hyderabad",
-#     "Role":"SDE"
+#     "Name":"KKAHMED",
+#     "RollNo":32,
+#     "Blood grp":"A+",
+#     "Branch":"CSE-CS",
+#     "Place":"SIDDIPET",
+#     "Role":"EH"
 # }
 # print(BioData)
 # # pop() : Removes the key value
@@ -119,12 +119,12 @@
 # Methods allow you to access dictionary data easily. 
 # keys(),values(), items()
 # BioData = {
-#     "Name":"Nandhan",
-#     "RollNo":66,
-#     "Blood grp":"B+",
-#     "Branch":"CSC",
-#     "Place":"Hyderabad",
-#     "Role":"SDE"
+#     "Name":"KKAHMED",
+#     "RollNo":32,
+#     "Blood grp":"A+",
+#     "Branch":"CSE-CS",
+#     "Place":"SIDDIPET",
+#     "Role":"EH"
 # }
 # # keys(): It prints only the keys of dictionary
 # print(BioData.keys()) #  n,r,blg,brn,plc,rol
@@ -142,29 +142,30 @@
 
 # Loops for Dictionary:
 # We can loop over keys,values,items(both).
-# GR = {
-#     "Name":"Kaveri",
-#     "RollNo":17,
-#     "Blood grp":"O-",
-#     "Branch":"CSC",
-#     "Place":"Kothagudem",
-#     "Role":"SDE"
+# CR = {
+#     "Name":"KKAHME",
+#     "RollNo":32,
+#     "Blood grp":"A+",
+#     "Branch":"CSE-CS",
+#     "Place":"SIDDIPET",
+#     "Role":"EH"
 # }
 # Loop through keys 
 # by-default the loop iterate the keys from the dictionary.
-# for i in GR:
+# for i in CR:
 #     print(i) # name,roll,bldgrp,braN,plc,role
 # The loop iterates the mentioned methods elements. 
-# for i in GR.keys():
+# for i in CR.keys():
 #     print(i) # name,roll,bldgrp,braN,plc,role
-# for i in GR.values():
-#     print(i) # kav,17,o-,kg,sde
-# for i in GR.items():
+# for i in CR.values():
+#     print(i) 
+# for i in CR.items():
 #     print(i)
 
 #  Nested  Dictionary:
 
 students = {
-    "CR":{"Name":"Nikhil","RollNo":16},
-    "GR":{"Name":"Kaveri","RollNo":17} }
+    "CR":{"Name":"KKAHMED","RollNo":32},
+    "GR":{"Name":"KKAHMED","RollNo":32} }
 print(students["CR"]["Name"])
+
